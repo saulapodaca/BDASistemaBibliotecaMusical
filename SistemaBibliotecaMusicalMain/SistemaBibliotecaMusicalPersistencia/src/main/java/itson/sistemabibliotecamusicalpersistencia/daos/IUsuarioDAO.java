028b1c619c959383f1dfd6e42b337707cf67a074
saulapodaca <saul.apodaca252020@potros.itson.edu.mx>
2025-07-08 04:02:28 -0700

    mock de usuarioDao obtenerGenerosNoDeseados

--- SistemaBibliotecaMusicalMain/SistemaBibliotecaMusicalPersistencia/src/main/java/itson/sistemabibliotecamusicalpersistencia/daos/IUsuarioDAO.java
+++ SistemaBibliotecaMusicalMain/SistemaBibliotecaMusicalPersistencia/src/main/java/itson/sistemabibliotecamusicalpersistencia/daos/IUsuarioDAO.java
@@ -3,9 +3,12 @@ package itson.sistemabibliotecamusicalpersistencia.daos;
 //@author SAUL ISAAC APODACA BALDENEGRO 00000252020
 
 import itson.sistemabibliotecamusicaldominio.UsuarioDominio;
+import itson.sistemabibliotecamusicaldominio.dtos.ActualizarGenerosUsuarioDTO;
 import itson.sistemabibliotecamusicaldominio.dtos.ModificarUsuarioDTO;
 import itson.sistemabibliotecamusicaldominio.dtos.RegistrarUsuarioDTO;
 import itson.sistemabibliotecamusicalpersistencia.excepciones.PersistenciaException;
+import java.util.List;
+import org.bson.types.ObjectId;
 
 
 
@@ -19,4 +22,9 @@ public interface IUsuarioDAO {
     public UsuarioDominio registrarUsuario(RegistrarUsuarioDTO nuevoUsuario) throws PersistenciaException;
     
     public UsuarioDominio modificarUsuario(ModificarUsuarioDTO usuarioModificado) throws PersistenciaException;
+    
+    public UsuarioDominio actualizarGenerosNoDeseados(ActualizarGenerosUsuarioDTO usuarioActualizar) throws PersistenciaException;
+    
+    public List<String> obtenerGenerosNoDeseados(ObjectId id) throws PersistenciaException;
+
 }
--- SistemaBibliotecaMusicalMain/SistemaBibliotecaMusicalPersistencia/src/main/java/itson/sistemabibliotecamusicalpersistencia/daos/implementaciones/UsuarioDAO.java
+++ SistemaBibliotecaMusicalMain/SistemaBibliotecaMusicalPersistencia/src/main/java/itson/sistemabibliotecamusicalpersistencia/daos/implementaciones/UsuarioDAO.java
@@ -3,10 +3,14 @@ package itson.sistemabibliotecamusicalpersistencia.daos.implementaciones;
 //@author SAUL ISAAC APODACA BALDENEGRO 00000252020
 
 import itson.sistemabibliotecamusicaldominio.UsuarioDominio;
+import itson.sistemabibliotecamusicaldominio.dtos.ActualizarGenerosUsuarioDTO;
 import itson.sistemabibliotecamusicaldominio.dtos.ModificarUsuarioDTO;
 import itson.sistemabibliotecamusicaldominio.dtos.RegistrarUsuarioDTO;
 import itson.sistemabibliotecamusicalpersistencia.daos.IUsuarioDAO;
 import itson.sistemabibliotecamusicalpersistencia.excepciones.PersistenciaException;
+import java.util.ArrayList;
+import java.util.List;
+import org.bson.types.ObjectId;
 
 
 public class UsuarioDAO implements IUsuarioDAO{
@@ -35,6 +39,22 @@ public class UsuarioDAO implements IUsuarioDAO{
     @Override
     public UsuarioDominio modificarUsuario(ModificarUsuarioDTO usuarioModificado) throws PersistenciaException {
         return new UsuarioDominio();
+    }
+
+    @Override
+    public UsuarioDominio actualizarGenerosNoDeseados(ActualizarGenerosUsuarioDTO usuarioActualizar) throws PersistenciaException {
+        UsuarioDominio usuario = new UsuarioDominio();
+        usuario.setId(usuarioActualizar.getId());
+        usuario.setGenerosNoDeseados(usuarioActualizar.getGenerosNoDeseados());
+        return usuario;
+    }
+
+    @Override
+    public List<String> obtenerGenerosNoDeseados(ObjectId id) throws PersistenciaException {
+        List<String> generos = new ArrayList<>();
+        generos.add("Rock");
+        generos.add("Trap");
+        return generos;
     }
 
     
