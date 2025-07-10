a4aa10fd433be62ece404dfd6c751bc37f02be05
saulapodaca <saul.apodaca252020@potros.itson.edu.mx>
2025-07-09 17:07:40 -0700

    camvio en favorito dominio

--- SistemaBibliotecaMusicalMain/SistemaBibliotecaMusicalDominio/src/main/java/itson/sistemabibliotecamusicaldominio/FavoritoDominio.java
+++ SistemaBibliotecaMusicalMain/SistemaBibliotecaMusicalDominio/src/main/java/itson/sistemabibliotecamusicaldominio/FavoritoDominio.java
@@ -21,7 +21,6 @@ public class FavoritoDominio {
     }
 
     public FavoritoDominio(TipoFavoritoEnum tipo, ObjectId referenciaFavorito) {
-        this.id = new ObjectId();
         this.tipo = tipo;
         this.referenciaFavorito = referenciaFavorito;
         this.fechaAgregado = new Date();
