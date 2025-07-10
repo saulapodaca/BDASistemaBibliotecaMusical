69346c4b316e63284805cc615d2b760961441fd3
Camila-Zubia <cami.zubia@gmail.com>
2025-07-09 20:24:18 -0700

    dtos documentados

--- SistemaBibliotecaMusicalMain/SistemaBibliotecaMusicalDominio/src/main/java/itson/sistemabibliotecamusicaldominio/dtos/ModificarUsuarioDTO.java
+++ SistemaBibliotecaMusicalMain/SistemaBibliotecaMusicalDominio/src/main/java/itson/sistemabibliotecamusicaldominio/dtos/ModificarUsuarioDTO.java
@@ -4,7 +4,10 @@ package itson.sistemabibliotecamusicaldominio.dtos;
 
 import org.bson.types.ObjectId;
 
-
+/**
+ * DTO que se utiliza para modificar ciertos atributos del usuario
+ * @author Camila Zubía
+ */
 public class ModificarUsuarioDTO {
 
     private ObjectId id;
@@ -12,9 +15,19 @@ public class ModificarUsuarioDTO {
     private String correo;
     private String imagen;
 
+    /**
+     * constructor vacio
+     */
     public ModificarUsuarioDTO() {
     }
 
+    /**
+     * constructor que inicializa los atributos
+     * @param id
+     * @param nombre
+     * @param correo
+     * @param imagen 
+     */
     public ModificarUsuarioDTO(ObjectId id, String nombre, String correo, String imagen) {
         this.id = id;
         this.nombre = nombre;
@@ -22,6 +35,11 @@ public class ModificarUsuarioDTO {
         this.imagen = imagen;
     }
 
+    /**
+     * getters y setters
+     * @return 
+     */
+    
     public ObjectId getId() {
         return id;
     }
